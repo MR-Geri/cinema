70f0add96c9e3b7de9ce623e4f0c7751c2840f15
MR-Geri <ilya.kamckine@ya.ru>
2020-11-11 20:43:53 +0300

    Вход под разными правами

--- Code/widgets.py
+++ Code/widgets.py
@@ -9,11 +9,19 @@ class Login(QMainWindow):
         super().__init__()
         uic.loadUi('../qt/login.ui', self)
         self.button_sign_in.clicked.connect(self.sign_in)
+        self.label_info.setText('Введите логин и пароль.')
+        self.line_login.textChanged.connect(self.update_line)
+        self.line_password.textChanged.connect(self.update_line)
+
+    def update_line(self):
+        self.label_info.setText('Введите логин и пароль.')
 
     def sign_in(self):
         accounts = json.load(open('../accounts.json'))
-        if accounts[self.line_login.text()] == self.line_password.text():
+        if accounts.get(self.line_login.text(), None) == self.line_password.text():
             print('Вход разрешён')
+        else:
+            self.label_info.setText('Неправильный логин или пароль.')
 
 
 class Form_login(QMainWindow):
