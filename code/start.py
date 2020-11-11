53f8834cd6e1c402f4839ca522eff70b4be4043b
MR-Geri <ilya.kamckine@ya.ru>
2020-11-11 18:13:40 +0300

    Форма входа в базу

--- code/start.py
+++ code/start.py
@@ -5,23 +5,43 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import *
 
 
+class Form_login(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('../qt/form_login.ui', self)
+        self.button_cashier.clicked.connect(self.cashier)
+        self.button_admin.clicked.connect(self.admin)
+
+    def cashier(self):
+        pass
+
+    def admin(self):
+        pass
+
+
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('../qt/start.ui', self)
         pix_map = QPixmap('../image/start.jpg')
         self.path_base_file = None
+        self.form_login = None
         self.image.setPixmap(pix_map.scaled(365, 400))
         self.button_create.clicked.connect(self.create_base)
         self.button_load.clicked.connect(self.load_base)
 
     def create_base(self):
-        pass
+        #
+        self.hide()
 
     def load_base(self):
         self.path_base_file = QFileDialog.getOpenFileName(
             self, 'Выбрать базу', '',
             'SQLite (*.sqlite);;Все файлы (*)')[0]
+        self.form_login = Form_login()
+        self.form_login.show()
+        #
+        self.hide()
 
 
 if __name__ == '__main__':
