39ec58df16f07bbd1f476c055cf35f271b768d74
MR-Geri <ilya.kamckine@ya.ru>
2020-11-30 18:09:09 +0300

    update

---
+++ dist/settings.py
@@ -0,0 +1,11 @@
+import os
+
+
+path_icon = os.path.abspath('../yandex.ico')
+path_accounts = os.path.abspath('../accounts.json')
+path_image_start = os.path.abspath('../image/start.jpg')
+path_temp = os.path.abspath('../temp/')
+path_ticket = os.path.abspath('../image/ticket.jpg')
+path_font = os.path.abspath('../My_Font/ticket.otf')
+path_password_font = os.path.abspath('../My_Font/password.otf')
+time_restart_session = 29  # minutes
